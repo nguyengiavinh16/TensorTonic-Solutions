84a4e861fcdd17d4f05873fe79e51abb46cda1da
Nguyễn Gia Vinh <Vinh.NG2416766@sis.hust.edu.vn>
2026-09-13 00:30:01 +0700

    Solve matrix-transpose

---
+++ matrix-transpose/matrix-transpose.py
@@ -0,0 +1,9 @@
+import numpy as np
+
+def matrix_transpose(A: list) -> np.ndarray:
+    """
+    Returns the transposed matrix as a NumPy array.
+    """
+    # Write code here
+    A = np.array(A)
+    return A.T
